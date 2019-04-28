1baafb049e5b72b87c7d4b0e8d8cde93f0f01681
Tom Milligan <tommilligan@users.noreply.github.com>
2019-04-28 21:06:46 +0100

    working tests for fifo

--- pytest_jobserver/plugin.py
+++ pytest_jobserver/plugin.py
@@ -1,3 +1,6 @@
+import os
+import stat
+
 import pytest
 
 
@@ -22,7 +25,32 @@ class JobserverPlugin(object):
         self.jobserver.write(token)
 
 
+def is_fifo(path):
+    return stat.S_ISFIFO(os.stat(path).st_mode)
+
+
+def is_rw_ok(path):
+    return os.access(path, os.R_OK | os.W_OK)
+
+
 def pytest_configure(config):
-    jobserver_file = config.getoption("jobserver", default=None)
-    if jobserver_file:
-        config.pluginmanager.register(JobserverPlugin(jobserver_file))
+    jobserver_path = config.getoption("jobserver", default=None)
+    if jobserver_path:
+        if os.path.exists(jobserver_path) is False:
+            raise pytest.UsageError(
+                "jobserver doesn't exist: {}".format(jobserver_path)
+            )
+
+        if is_fifo(jobserver_path) is False:
+            raise pytest.UsageError(
+                "jobserver is not a fifo/named pipe: {}".format(jobserver_path)
+            )
+
+        if is_rw_ok(jobserver_path) is False:
+            raise pytest.UsageError(
+                "jobserver is not read/writeable to current user: {}".format(
+                    jobserver_path
+                )
+            )
+
+        config.pluginmanager.register(JobserverPlugin(jobserver_path))
--- tests/test_jobserver.py
+++ tests/test_jobserver.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+import os
 
 
 def test_help_message(testdir):
@@ -7,21 +7,58 @@ def test_help_message(testdir):
     result.stdout.fnmatch_lines(["jobserver:", "*--jobserver*"])
 
 
-def test_plugin_setup(testdir):
+def test_server_fifo(testdir):
     testdir.makepyfile(
         """
         import pytest
 
         def test_plugin_setup(request):
-            assert 'jobserver.pipe' == request.config.getoption('jobserver')
+            assert 'jobserver_named_pipe' == request.config.getoption('jobserver')
     """
     )
-    testdir.makefile(".pipe", jobserver="X")
+    # Create our fifo and write a byte into it
+    fifo_path = os.path.join(testdir.tmpdir, "jobserver_named_pipe")
+    os.mkfifo(fifo_path)
+    fifo = os.open(fifo_path, os.O_RDWR | os.O_NONBLOCK)
+    os.write(fifo, b"X")
 
-    result = testdir.runpytest("-v", "--jobserver", "jobserver.pipe")
+    # Run our test
+    result = testdir.runpytest("-v", "--jobserver", "jobserver_named_pipe")
 
-    # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines(["*::test_plugin_setup PASSED*"])
-
-    # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0
+
+
+def test_server_not_found(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+
+        def test_pass(request):
+            pass
+    """
+    )
+    # Run our test
+    result = testdir.runpytest("-v", "--jobserver", "non_existent_file")
+
+    result.stderr.fnmatch_lines(["ERROR: jobserver doesn't exist: non_existent_file"])
+    assert result.ret == 4
+
+
+def test_server_not_fifo(testdir):
+    testdir.makefile(".txt", jobserver="X")
+    testdir.makepyfile(
+        """
+        import pytest
+
+        def test_pass(request):
+            pass
+    """
+    )
+    # Run our test
+    result = testdir.runpytest("-v", "--jobserver", "jobserver.txt")
+
+    result.stderr.fnmatch_lines(
+        ["ERROR: jobserver is not a fifo/named pipe: jobserver.txt"]
+    )
+    assert result.ret == 4
